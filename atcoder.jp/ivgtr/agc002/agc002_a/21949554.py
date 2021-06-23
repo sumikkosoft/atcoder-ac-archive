df4379badd9eefe2df1bf3ef4ed5f33f3b65cb82
ivgtr <ivgtr.me@gmail.com>
2021-06-23 06:15:42 +0000

    [AC] agc002 agc002_a

---
+++ atcoder.jp/ivgtr/agc002/agc002_a/21949554.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+
+
+if a <= 0 and b >= 0:
+    print("Zero")
+elif a > 0:
+    print("Positive")
+else:
+    if abs(a - b) % 2 == 0:
+        print("Negative")
+    else:
+        print("Positive")
