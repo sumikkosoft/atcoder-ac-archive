11a2c2f085526f4979d76ed79fdf0c7fe31261ba
ivgtr <ivgtr.me@gmail.com>
2021-06-25 02:13:49 +0000

    [AC] abc145 abc145_c

---
+++ atcoder.jp/ivgtr/abc145/abc145_c/22891527.py
@@ -0,0 +1,15 @@
+import itertools
+import math
+
+N = int(input())
+tmp = list(itertools.permutations(range(N), 2))
+
+A = [list(map(int, input().split())) for _ in range(N)]
+ans = 0
+
+for i in tmp:
+    ans += (
+        math.sqrt((A[i[0]][0] - A[i[1]][0]) ** 2 + (A[i[0]][1] - A[i[1]][1]) ** 2) / N
+    )
+
+print(ans)
