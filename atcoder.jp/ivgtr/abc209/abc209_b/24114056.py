a89c327857857b78ca66bbbbbc713bf25b8c8056
ivgtr <ivgtr.me@gmail.com>
2021-07-10 15:23:56 +0000

    [AC] abc209 abc209_b

---
+++ atcoder.jp/ivgtr/abc209/abc209_b/24114056.py
@@ -0,0 +1,15 @@
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+for i in range(N):
+    if i % 2 == 0:
+        X -= A[i]
+    else:
+        X -= A[i] - 1
+
+    if X < 0:
+        print("No")
+        exit()
+
+
+print("Yes")
