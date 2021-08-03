5e0b7c11cb3fa2d9f6d8de7ec177cc99638d2804
ivgtr <ivgtr.me@gmail.com>
2021-08-03 15:24:08 +0000

    [AC] abc016 abc016_1

---
+++ atcoder.jp/ivgtr/abc016/abc016_1/24744403.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		a int
+		b int
+	)
+
+	fmt.Scan(&a, &b)
+
+	if a%b == 0 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
