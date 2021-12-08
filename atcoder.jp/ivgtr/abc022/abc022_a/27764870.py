b9e48b70a6f9df900818a79d8ccc3a6dba89abf5
ivgtr <ivgtr.me@gmail.com>
2021-12-08 15:17:30 +0000

    [AC] abc022 abc022_a

---
+++ atcoder.jp/ivgtr/abc022/abc022_a/27764870.py
@@ -0,0 +1,15 @@
+N, S, T = map(int, input().split())
+W = int(input())
+
+ans = 0
+if S <= W <= T:
+    ans += 1
+
+
+for i in range(N - 1):
+    A = int(input())
+    W += A
+    if S <= W <= T:
+        ans += 1
+
+print(ans)
